8f03ef61579add93c6d31addd36b8921e04ce570
david_tojo <david_tojo@hotmail.es>
2024-05-16 17:59:57 +0200

    ci: :construction_worker: Add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [preact({ compat: true })]
+  integrations: [preact({
+    compat: true
+  })],
+  adapter: vercel()
 });
